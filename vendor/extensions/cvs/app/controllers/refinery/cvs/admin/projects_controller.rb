ef484162cf8588decee342894eb865c23feb6e54
Juan David Giraldo Rodriguez <jdgiraldo88@hotmail.com>
2014-08-15 17:33:16 -0500

    Relating resources in a extension

--- vendor/extensions/cvs/app/controllers/refinery/cvs/admin/projects_controller.rb
+++ vendor/extensions/cvs/app/controllers/refinery/cvs/admin/projects_controller.rb
@@ -3,9 +3,20 @@ module Refinery
     module Admin
       class ProjectsController < ::Refinery::AdminController
 
+        before_filter :find_all_cvs, :only => [:show, :new]
+
         crudify :'refinery/cvs/project',
                 :title_attribute => 'name',
                 :xhr_paging => true
+
+        protected
+          def find_all_cvs
+            @cvs = Refinery::Cvs::Cv.all
+          end
+
+          def project_params
+            params.require(:project).permit(:name, :url, :description, :position)
+          end
 
       end
     end
--- vendor/extensions/cvs/app/controllers/refinery/cvs/cvs_controller.rb
+++ vendor/extensions/cvs/app/controllers/refinery/cvs/cvs_controller.rb
@@ -13,6 +13,7 @@ module Refinery
 
       def show
         @cv = Cv.find(params[:id])
+        @projects = Project.where(cv_id=@cv.id)
 
         # you can use meta fields from your model instead (e.g. browser_title)
         # by swapping @page for @cv in the line below:
